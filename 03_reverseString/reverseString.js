1b3e2e71eba254d558dd7d5e95923aa3e7e71986
lemonroot <79768526+lemonroot@users.noreply.github.com>
2022-05-28 13:56:16 -0400

    03_reverseString init
    Initial test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const string = 'hello there';
+reverseString(string);
 
-};
+function reverseString(string){
+    let stringArray = [];
+    let reversedString = string[string.length - 1];
+    for(let i = 0; i < (string.length); i++){
+        stringArray[i] = string[i];
+    }
+
+    for(let i = (string.length - 2); i >= 0; i--){
+        reversedString += stringArray[i];
+    }
+    console.log(stringArray);
+    console.log(reversedString);
+    return reversedString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
