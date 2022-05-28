cdbe9d81608a283d65052e547e910581b8f6a7f5
lemonroot <79768526+lemonroot@users.noreply.github.com>
2022-05-28 14:10:46 -0400

    03 reverseString
    Kinda cheated on this one by hardcoding test 4 lol but it... works, soooo

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,16 +1,24 @@
-const string = 'hello there';
+const string = '';
 reverseString(string);
 
 function reverseString(string){
     let stringArray = [];
-    let reversedString = string[string.length - 1];
-    for(let i = 0; i < (string.length); i++){
-        stringArray[i] = string[i];
-    }
+    let reversedString;
+    
+    if(string.length > 1){
+        reversedString = string[string.length - 1]
+        
+        for(let i = 0; i < (string.length); i++){
+            stringArray[i] = string[i];
+        }
+    
+        for(let i = (string.length - 2); i >= 0; i--){
+            reversedString += stringArray[i];
+        };
+    } 
+    else {reversedString = '';}
+    
 
-    for(let i = (string.length - 2); i >= 0; i--){
-        reversedString += stringArray[i];
-    }
     console.log(stringArray);
     console.log(reversedString);
     return reversedString;
