9a6c2801bde10ca4c8ae704bcb76eda42b4f3257
lemonroot <79768526+lemonroot@users.noreply.github.com>
2022-05-27 20:02:28 -0400

    02 repeatStrings
    All tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+let baseNum = Math.random(0, 1);
+let num = Math.floor(baseNum * 1000);
 
-};
+repeatString('hey', num);
+
+function repeatString(string, num){
+    let finString = string;
+    if(num > 0){
+        for (let i = 1; i < num; i++){
+            finString+=string;
+        }
+    } else if (num == 0){
+      finString = '';  
+    } else {
+        finString = "ERROR";
+    }
+    
+    return finString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
